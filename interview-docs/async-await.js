24e160ee01068b794f7463e60bfe125e82707e5c
Ranajit Das <dasranajit410@gmail.com>
2024-11-08 00:17:54 +0530

    if there is two promise

--- interview-docs/async-await.js
+++ interview-docs/async-await.js
@@ -1,26 +1,37 @@
-let promiseData = new Promise((resolve, reject) => {
+let promiseData1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Hello world");
+    resolve("Promise data 1 resolved");
+  }, 5000);
+});
+let promiseData2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise data 2 resolved");
   }, 2000);
 });
 
 function getDataUsingThenCatch() {
-  promiseData.then((res) => {
+  promiseData1.then((res) => {
+    console.log(res);
+  });
+  promiseData2.then((res) => {
     console.log(res);
   });
   console.log("First");
 }
 getDataUsingThenCatch();
 // Output: First
-//Hello world (after 2 sec)
+//Promise data 2 resolved (after 2 sec)
+//Promise data 1 resolved (after 5 sec)
 
 //using async/await
 async function getDataUsingAsyncAwait() {
-  let data = await promiseData;
-  console.log(data);
+  let data1 = await promiseData1;
+  console.log(data1);
+  let data2 = await promiseData2;
+  console.log(data2);
   console.log("First");
 }
 getDataUsingAsyncAwait();
-//output: after 2 sec
-//Hello World
+//Promise data 1 resolved (after 5 sec)
+//Promise data 2 resolved (after 5 sec simultaneously)
 //First
