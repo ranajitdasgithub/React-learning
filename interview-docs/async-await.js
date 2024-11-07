819997ce637326e987c5f18504a63dcf9f9d9f6e
Ranajit Das <dasranajit410@gmail.com>
2024-11-08 00:07:19 +0530

    using async await vs then and catch

--- interview-docs/async-await.js
+++ interview-docs/async-await.js
@@ -1,11 +1,26 @@
-let getData = async function () {
-  let data = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Hello world");
-    }, 2000);
-  });
-  return data
-};
+let promiseData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hello world");
+  }, 2000);
+});
 
-console.log("First");
-console.log(getData());
+function getDataUsingThenCatch() {
+  promiseData.then((res) => {
+    console.log(res);
+  });
+  console.log("First");
+}
+getDataUsingThenCatch();
+// Output: First
+//Hello world (after 2 sec)
+
+//using async/await
+async function getDataUsingAsyncAwait() {
+  let data = await promiseData;
+  console.log(data);
+  console.log("First");
+}
+getDataUsingAsyncAwait();
+//output: after 2 sec
+//Hello World
+//First
